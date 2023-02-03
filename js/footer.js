57d448531da68096d9f9b4d5557b5a7cf415f181
Ngi Bujri <nbujri7@gmail.com>
2023-02-03 17:25:24 -0500

    create footer component

--- js/footer.js
+++ js/footer.js
@@ -2,3 +2,21 @@
 // Author: Ngi Bujri
 // Date: 3 February 2023
 // Description: Footer component
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+
+    const footer = `
+    <footer class="footer">
+      <div>
+        Developed by <a href="https://github.com/nbujri/web-330-portfolio" target="_blank">Ngi Bujri</a>
+      </div>
+    </footer>
+    `;
+
+    this.innerHTML = footer;
+  }
+}
+
+customElements.define("page-footer", Footer);
