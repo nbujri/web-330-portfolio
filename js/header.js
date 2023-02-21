5a1c21d49de0b6a9b4c28209c4cf86b4eb662de6
Ngi Bujri <nbujri7@gmail.com>
2023-02-21 14:20:23 -0500

    change paths for nav links

--- js/header.js
+++ js/header.js
@@ -12,12 +12,12 @@ class Header extends HTMLElement {
       <div class="logo">Ngi Bujri</div>
       <nav class="nav-links">
         <a href="/" class="nav-link">Home</a>
-        <a href="/resume.html" class="nav-link">Resume</a>
-        <a href="/projects.html" class="nav-link">Projects</a>
-        <a href="/db-diagrams.html" class="nav-link">DB Diagrams</a>
-        <a href="/api-unit-tests.html" class="nav-link">API Unit Tests</a>
-        <a href="/bucket-list.html" class="nav-link">Bucket List</a>
-        <a href="/about.html" class="nav-link">About</a>
+        <a href="../resume.html" class="nav-link">Resume</a>
+        <a href="../projects.html" class="nav-link">Projects</a>
+        <a href="../db-diagrams.html" class="nav-link">DB Diagrams</a>
+        <a href="../api-unit-tests.html" class="nav-link">API Unit Tests</a>
+        <a href="../bucket-list.html" class="nav-link">Bucket List</a>
+        <a href="../about.html" class="nav-link">About</a>
       </nav>
     </header>
     `;
